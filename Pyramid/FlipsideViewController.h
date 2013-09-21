29a348c564c8fc4241593638baa6fca63d8f2e7e
pwansch <pwansch@me.com>
2013-09-20 18:19:06 -0700

    Initial Commit

---
+++ Pyramid/FlipsideViewController.h
@@ -0,0 +1,23 @@
+//
+//  FlipsideViewController.h
+//  Pyramid
+//
+//  Created by Peter Wansch on 9/20/13.
+//  Copyright (c) 2013 Peter Wansch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FlipsideViewController;
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
+@interface FlipsideViewController : UIViewController
+
+@property (weak, nonatomic) id <FlipsideViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ Pyramid/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  Pyramid
+//
+//  Created by Peter Wansch on 9/20/13.
+//  Copyright (c) 2013 Peter Wansch. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate, UIPopoverControllerDelegate>
+
+@property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
+
+@end
